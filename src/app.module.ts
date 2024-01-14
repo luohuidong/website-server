bf87a0f5ffa14b4c43443159585198b464e854c8
luohuidong <luohuidong01@126.com>
2024-01-14 23:37:28 +0800

    load .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [ConfigModule.forRoot(), PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
